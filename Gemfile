48c308b3c3ac866fd350667e51b68c45e30ae359
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-06-25 14:30:54 +0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+group :assets do
+  gem 'coffee-rails'
+  gem 'jquery-rails'
+  gem 'sass-rails'
+  gem 'uglifier'
+end
+
+group :default do
+  gem 'rails'
+end
+
+group :test do
+  gem 'sqlite3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarketResearch::Application.config.secret_token = 'd5cf80265e66ca971837b32437fcc91661cfa86b5877971de5be6ff25fc678638fa788ec7dc0131a1bfb62ffd69c60e9f9f2f2dd41e03ecfe14de57513b98fa9'
