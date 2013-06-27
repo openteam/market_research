34d96457b1d7f58a8f6aaa264c3f89333df4c2d6
tab <tukhfatullin@gmail.com>
2013-06-27 13:21:36 +0700

    Fix airbrake initializers

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,11 @@
-Airbrake.configure do |config|
-  config.api_key = Settings['errors']['key']
+if Settings['errors.key'].present?
+  Airbrake.configure do |config|
+    config.api_key = Settings['errors.key']
+
+    URI.parse(Settings['errors.url']).tap do | url |
+      config.host   = url.host
+      config.port   = url.port
+      config.secure = url.scheme.inquiry.https?
+    end
+  end
 end
